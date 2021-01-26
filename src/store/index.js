2fabb8dba35b43d3c8bbe40929b389ef75a03017
Stacy Burris <stacy1burris@gmail.com>
2021-01-26 15:26:44 -0800

    broken

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,10 @@ import { createStore, combineReducers } from 'redux';
 // This dependency enables the Redux Dev Tools in your Chrome Console.
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import activateCategory from './categories';
-import activateProducts from './categories'
+import category from './categories';
+//import products from './products';
 // combine our reducers - right now it isn't necessary, however, you usually will have more than one and it will be.
-let reducers = combineReducers({ activateCategory, activateProducts });
+let reducers = combineReducers({ category });
 
 // finally, we get to actually create the store
 const store = () => {
